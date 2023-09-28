cfc0f62c2cf32f09de59a955d9360ebd0952a3c7
arcanium <unicomparteargentina@gmail.com>
2023-09-28 17:23:00 -0300

    changes on database port

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const server = require('./src/server')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8004
 
 server.listen(PORT, () => {
     console.log(`Server DB running on PORT: ${PORT}`)
